c3592bd9f979238c4a1972e324c65e9cdd1f847a
Matthew Yang <matthewyang434@gmail.com>
2023-06-27 19:35:04 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+let hq = 42;
+const feetPerBlock = 264;
+
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(hq - blocks);
+}
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * feetPerBlock;
+}
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((destination - start) * feetPerBlock);
+}
+function calculatesFarePrice(start, destination) {
+    let fare = 0;
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        fare = 0;
+    } 
+    else if (400 < distance && distance < 2000) {
+        fare = (distance - 400) * .02;
+    }
+    else if (2000 < distance && distance < 2500) {
+        fare = 25;
+    } else {return 'cannot travel that far'};
+    return fare;
+}
+
